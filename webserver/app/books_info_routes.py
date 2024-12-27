c6c752787ffc421193d41f8e5d217bd2215130c9
RuntimeErrorGr <andrei_liviu.labau@stud.acs.upb.ro>
2024-12-28 01:33:02 +0200

    feat: add authorization policy for books-information

--- webserver/app/books_info_routes.py
+++ webserver/app/books_info_routes.py
@@ -6,24 +6,43 @@ from datetime import datetime
 books_info_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
+class NoPermissionError(Exception):
+    """Raised when the user does not have permission to access the requested resource."""
+
+
+def make_authenticated_request(url):
+    access_token = session.get("Authorization")
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    try:
+        response = requests.get(url, timeout=5, headers=headers)
+        if response.status_code in [401, 403]:
+            logging.error(
+                f"Failed to fetch data: {response.status_code} {response.text}"
+            )
+            raise NoPermissionError
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error connecting to books service: {e}")
+        return None
+
+
 @books_info_bp.route("", methods=["GET"])
 def index():
     try:
-        books_service_url = current_app.config["BOOKS_SERVICE_URL"]
-        response = requests.get(f"{books_service_url}/books", timeout=5)
-
-        if response.status_code == 200:
-            books_data = response.json()
-            logging.info(f"Fetched books data: {books_data}")
-            return jsonify(books_data)
-        else:
-            logging.error(
-                f"Failed to fetch books: {response.status_code} {response.text}"
-            )
-            return (
-                jsonify({"error": "Failed to fetch books data."}),
-                response.status_code,
-            )
+        books_data = make_authenticated_request(
+            f"{current_app.config['BOOKS_SERVICE_URL']}/books"
+        )
+        logging.info(f"Fetched books data: {books_data}")
+        return jsonify(books_data)
+    except NoPermissionError:
+        return (
+            jsonify({"error": "You do not have permission to access this resource."}),
+            403,
+        )
     except requests.exceptions.RequestException as e:
         logging.error(f"Error connecting to books service: {e}")
         return jsonify({"error": "Error fetching books data."}), 500
@@ -32,24 +51,20 @@ def index():
 @books_info_bp.route("/<isbn>", methods=["GET"])
 def get_book(isbn):
     try:
-        books_service_url = current_app.config["BOOKS_SERVICE_URL"]
-        response = requests.get(f"{books_service_url}/books/{isbn}", timeout=5)
+        book_data = make_authenticated_request(
+            f"{current_app.config['BOOKS_SERVICE_URL']}/books/{isbn}"
+        )
         username = session.get("username")
         role = session.get("role")
-        if response.status_code == 200:
-            book_data = response.json()
-            logging.info(f"Fetched book data: {book_data}")
-            return render_template(
-                "book_page.html", book=book_data, username=username, role=role
-            )
-        else:
-            logging.error(
-                f"Failed to fetch book: {response.status_code} {response.text}"
-            )
-            return (
-                jsonify({"error": "Failed to fetch book data."}),
-                response.status_code,
-            )
+        logging.info(f"Fetched book data: {book_data}")
+        return render_template(
+            "book_page.html", book=book_data, username=username, role=role
+        )
+    except NoPermissionError:
+        return (
+            jsonify({"error": "You do not have permission to access this resource."}),
+            403,
+        )
     except requests.exceptions.RequestException as e:
         logging.error(f"Error connecting to books service: {e}")
         return jsonify({"error": "Error fetching book data."}), 500
@@ -73,23 +88,17 @@ def get_book_reviews(isbn):
         return transformed_data
 
     try:
-        books_service_url = current_app.config["BOOKS_SERVICE_URL"]
-        response = requests.get(
-            f"{books_service_url}/reviews/by-isbn/{isbn}", timeout=5
+        json_response = make_authenticated_request(
+            f"{current_app.config['BOOKS_SERVICE_URL']}/reviews/by-isbn/{isbn}"
         )
-
-        if response.status_code == 200:
-            reviews_data = transform_reviews(response.json())
-            logging.info(f"Fetched reviews data: {reviews_data}")
-            return jsonify(reviews_data)
-        else:
-            logging.error(
-                f"Failed to fetch reviews: {response.status_code} {response.text}"
-            )
-            return (
-                jsonify({"error": "Failed to fetch reviews data."}),
-                response.status_code,
-            )
+        reviews_data = transform_reviews(json_response)
+        logging.info(f"Fetched reviews data: {reviews_data}")
+        return jsonify(reviews_data)
+    except NoPermissionError:
+        return (
+            jsonify({"error": "You do not have permission to access this resource."}),
+            403,
+        )
     except requests.exceptions.RequestException as e:
         logging.error(f"Error connecting to books service: {e}")
         return jsonify({"error": "Error fetching reviews data."}), 500
