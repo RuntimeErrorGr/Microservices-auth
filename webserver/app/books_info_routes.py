9d96dec2e81e8378c9bb695eaed71164613a9698
RuntimeErrorGr <andrei_liviu.labau@stud.acs.upb.ro>
2024-12-27 23:00:01 +0200

    feat: add reviews

--- webserver/app/books_info_routes.py
+++ webserver/app/books_info_routes.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, jsonify, current_app, render_template, session
 import requests
 import logging
+from flask import Blueprint, jsonify, current_app, render_template, session
+from datetime import datetime
 
-books_info_bp = Blueprint("books", __name__)
+books_info_bp = Blueprint("books", __name__, url_prefix="/books")
 
 
-@books_info_bp.route("/books", methods=["GET"])
+@books_info_bp.route("", methods=["GET"])
 def index():
     try:
         books_service_url = current_app.config["BOOKS_SERVICE_URL"]
@@ -23,13 +24,12 @@ def index():
                 jsonify({"error": "Failed to fetch books data."}),
                 response.status_code,
             )
-
     except requests.exceptions.RequestException as e:
         logging.error(f"Error connecting to books service: {e}")
         return jsonify({"error": "Error fetching books data."}), 500
 
 
-@books_info_bp.route("/books/<isbn>", methods=["GET"])
+@books_info_bp.route("/<isbn>", methods=["GET"])
 def get_book(isbn):
     try:
         books_service_url = current_app.config["BOOKS_SERVICE_URL"]
@@ -50,7 +50,46 @@ def get_book(isbn):
                 jsonify({"error": "Failed to fetch book data."}),
                 response.status_code,
             )
-
     except requests.exceptions.RequestException as e:
         logging.error(f"Error connecting to books service: {e}")
         return jsonify({"error": "Error fetching book data."}), 500
+
+
+@books_info_bp.route("/<isbn>/reviews", methods=["GET"])
+def get_book_reviews(isbn):
+
+    def transform_reviews(input_data):
+        transformed_data = []
+        for review in input_data:
+            transformed_data.append(
+                {
+                    "reviewer": review["user"]["username"],
+                    "text": review["reviewText"],
+                    "date": datetime.fromisoformat(review["reviewDate"]).strftime(
+                        "%Y-%m-%d %H:%M:%S"
+                    ),
+                }
+            )
+        return transformed_data
+
+    try:
+        books_service_url = current_app.config["BOOKS_SERVICE_URL"]
+        response = requests.get(
+            f"{books_service_url}/reviews/by-isbn/{isbn}", timeout=5
+        )
+
+        if response.status_code == 200:
+            reviews_data = transform_reviews(response.json())
+            logging.info(f"Fetched reviews data: {reviews_data}")
+            return jsonify(reviews_data)
+        else:
+            logging.error(
+                f"Failed to fetch reviews: {response.status_code} {response.text}"
+            )
+            return (
+                jsonify({"error": "Failed to fetch reviews data."}),
+                response.status_code,
+            )
+    except requests.exceptions.RequestException as e:
+        logging.error(f"Error connecting to books service: {e}")
+        return jsonify({"error": "Error fetching reviews data."}), 500
